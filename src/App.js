af02fe22827eb6e36bc59b389d20bcf85d4043d8
Autumn Parker <agmparker@gmail.com>
2023-03-18 12:04:48 -0600

    Import Card Component
    - Imported the new card component and deleted duplicate code that was transferred to new file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import Card from './components/Card';
 
 // Create an array with the images for the front of the cards
 const cardImages = [
@@ -23,22 +24,22 @@ function App() {
   // Hold card state
   // Once cards are shuffled the state of the cards will be updated with useState
   const [cards, setCards] = useState([])
-// Function duplicates each card to make a pair within the game
-// Randomize the card order
-// Apply id to the cards to be used within my grid
-const shuffleCards = () => {
-  // Adds my array of images TWICE to create pairs in a new array
-  const shuffledCards = [...cardImages, ...cardImages]
-  // Sorts the cards to have a pair and using math random makes them have different orders in the array so finding a pair is more challenging
-  .sort(() => Math.random() - 0.5)
-  // Random id is created on each object - card
-  .map((card) => ({ ...card, id: Math.random()}))
-  // When function is called the cards will be shuffled and then updated within the setCards state
-  setCards(shuffledCards)
-}
-// Test
-// Test is working!
-console.log(cards);
+  // Function duplicates each card to make a pair within the game
+  // Randomize the card order
+  // Apply id to the cards to be used within my grid
+  const shuffleCards = () => {
+    // Adds my array of images TWICE to create pairs in a new array
+    const shuffledCards = [...cardImages, ...cardImages]
+      // Sorts the cards to have a pair and using math random makes them have different orders in the array so finding a pair is more challenging
+      .sort(() => Math.random() - 0.5)
+      // Random id is created on each object - card
+      .map((card) => ({ ...card, id: Math.random() }))
+    // When function is called the cards will be shuffled and then updated within the setCards state
+    setCards(shuffledCards)
+  }
+  // Test
+  // Test is working!
+  console.log(cards);
   return (
     <div className="App">
       <h1>ACNH Memory Match</h1>
@@ -50,14 +51,8 @@ console.log(cards);
         {/* Map through cards state */}
         {cards.map(card => (
           // Parent needs key prop - use id from shuffled cards
-          <div key={card.id}>
-            <div>
-              {/* Front and back of card here to display in grid */}
-              {/* Use property name on each card from cards image array */}
-              <img className="card-front" src={card.src} alt="card front"/>
-              <img className="card-back" src="/images/card-bg.png" alt="card back" />
-            </div>
-          </div>
+          // Send card prop into component so new component has access
+          <Card  key={card.id} card={card}/>
         ))}
       </div>
     </div>
