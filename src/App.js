ab435bab76d42c570a5bf05a42f43da9d5409f66
Autumn Parker <agmparker@gmail.com>
2023-03-18 10:19:21 -0600

    Create Card Grid
    - Getting the card grid set up so they can be displayed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 // Create an array with the images for the front of the cards
 const cardImages = [
   // Object with image source property
   // Add more after testing that image displays
-  {"src": "/images/apple.png"},
+  { "src": "/images/apple.png" },
 ]
 
 function App() {
+
   return (
     <div className="App">
       <h1>ACNH Memory Match</h1>
       <button>Play</button>
+      <div className="card-grid">
+        {/* Create card grid */}
+        { }
+      </div>
     </div>
   );
 }
