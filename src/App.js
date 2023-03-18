afb546dbf34a7ee549a0b57ec582ce66e228967f
Autumn Parker <agmparker@gmail.com>
2023-03-18 11:44:56 -0600

    Cards Display!!!!
    - The cards display on the screen and shuffle when the play button is clicked!!
    - Need to add styling and functionality so the cards can flip when clicked (back to front view)

--- src/App.js
+++ src/App.js
@@ -52,7 +52,10 @@ console.log(cards);
           // Parent needs key prop - use id from shuffled cards
           <div key={card.id}>
             <div>
-              
+              {/* Front and back of card here to display in grid */}
+              {/* Use property name on each card from cards image array */}
+              <img className="card-front" src={card.src} alt="card front"/>
+              <img className="card-back" src="/images/card-bg.png" alt="card back" />
             </div>
           </div>
         ))}
